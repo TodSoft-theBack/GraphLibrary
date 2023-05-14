b1baed813e6a85506c863064ec113c6843af3f4e
Тодор Тодоров <toshkofff19@gmail.com>
2023-05-14 20:00:02 +0300

    Added a utility class

--- Program.cs
+++ Program.cs
@@ -1,25 +1,11 @@
-﻿using System.Collections.Generic;
-
-namespace GraphLibrary
+﻿namespace GraphLibrary
 {
     class GrapthLibrary
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Starting graph reader...\n");
-
-            Console.Write("Enter an edge: ");
-            string? line = Console.ReadLine()?.Trim();
-            IGraph<char> graph = new GraphLE<char>();
-            while (!string.IsNullOrEmpty(line) && line != "\n")
-            {
-                char[] verteces = line.Split().Select(str => str[0]).ToArray();
-                graph.AddEdge(verteces[0], verteces[1]);
-                Console.Write("Enter an edge: ");
-                line = Console.ReadLine()?.Trim();
-            }
-            
-            Console.WriteLine("\nClosing graph reader...");
+            GrapthReader reader = new GrapthReader();
+            reader.RunReader();
         }
     }
 }
